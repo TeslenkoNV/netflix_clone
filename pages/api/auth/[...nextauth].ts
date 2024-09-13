cdf9c1c7bd99306b59bb55cb95324dd84970799e
TeslenkoNV <natella911@mail.ru>
2024-09-14 01:47:06 +0300

    Favorites and main

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,14 +1,12 @@
-import NextAuth from "next-auth/next";
+import NextAuth, { AuthOptions } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import prismadb from '@/lib/prismadb';
 import { compare } from 'bcrypt';
-
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
-
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 
-export default NextAuth({
+export const authOptions: AuthOptions = ({
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_ID || '',
@@ -61,11 +59,9 @@ export default NextAuth({
     },
     debug: process.env.NODE_ENV === 'development',
     adapter: PrismaAdapter(prismadb),
-    session: {
-        strategy: 'jwt',
-    },
-    jwt: {
-        secret: process.env.NEXTAUTH_JWT_SECRET,
-    },
+    session: { strategy: 'jwt' },
+    jwt: { secret: process.env.NEXTAUTH_JWT_SECRET, },
     secret: process.env.NEXTAUTH_SECRET,
 })
+
+export default NextAuth(authOptions);
--- pages/api/movies/index.ts
+++ pages/api/movies/index.ts
@@ -1,14 +1,19 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import serverAuth from '@/lib/serverAuth';
+
+import prismadb from '@/lib/prismadb'
+import serverAuth from "@/lib/serverAuth";
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method !== 'GET') {
-        return res.status(405).end();
+        return res.status(405). end();
     }
     try {
-        const { currentUser } = await serverAuth(req, res);
-        return res.status(200).json(currentUser);
+        await serverAuth(req, res)
+        const movies = await prismadb.movie.findMany();
+        return res.status(200).json(movies);
     } catch (error) {
         console.log(error);
         return res.status(400).end();
     }
 }
+
